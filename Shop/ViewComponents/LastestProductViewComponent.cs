305478aaf3365a7a797bb333b6257b07c4c05d2c
Nguyen Hoang Chuong <chuongnh.hcm@gmail.com>
2018-07-04 20:03:09 +0700

    init app

---
+++ Shop/ViewComponents/LastestProductViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.ViewComponents {
+    public class LastestProductViewComponent : ViewComponent {
+
+        private readonly ApplicationDbContext _context;
+
+        public LastestProductViewComponent(ApplicationDbContext context) {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke() {
+
+            var models = _context.Products.OrderByDescending(x => x.CreatedDate).Take(2).ToList();
+            return View(models);
+        }
+    }
+}
