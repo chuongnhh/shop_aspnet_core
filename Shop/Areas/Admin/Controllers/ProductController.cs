dc5ed9d079f255e158cf1005771f7dff543eb2ec
Nguyen Hoang Chuong <chuongnh.hcm@gmail.com>
2018-07-04 19:00:38 +0700

    init app

--- Shop/Areas/Admin/Controllers/ProductController.cs
+++ Shop/Areas/Admin/Controllers/ProductController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -11,7 +13,7 @@ namespace Shop.Areas.Admin.Controllers {
     [Area("Admin")]
     public class ProductController : Controller {
 
-        private readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context; 
 
         public ProductController(ApplicationDbContext context) {
             _context = context;
@@ -42,27 +44,56 @@ namespace Shop.Areas.Admin.Controllers {
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Details(int id, [Bind("Id,Name,CategoryProductId,Price,Quantity,Display,Description,ImageUrl")] Product model) {
+        public async Task<IActionResult> Details(int id, [Bind("Id,Name,CategoryProductId,Price,Quantity,Display,Description,ImageUrl")] Product model, IFormFile ImageUrl) {
 
             if (ModelState.IsValid) {
+
+                string fileName = "";
+                // 0. Kiểm tra xem người dùng có chọn file không?
+                if (ImageUrl != null && ImageUrl.Length != 0) {
+                    // 1. Chọn nơi lưu file
+                    string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files");
+                    // 2. nếu chưa có, thì tạo mới
+                    if (!Directory.Exists(path)) {
+                        Directory.CreateDirectory(path);
+                    }
+                    // 3. Lấy tên file
+                    fileName = ImageUrl.FileName;
+                    // 4. Lấy đường dẫn và tên file
+                    var filePath = Path.Combine(path, fileName);
+
+                    // 5. Tiến hành lưu file
+                    using (var stream = new FileStream(filePath, FileMode.Create)) {
+                        await ImageUrl.CopyToAsync(stream);
+                    }
+                }
+
                 if (ProductExists(id) == false) {
 
                     model.CreatedDate = DateTime.Now;
+
+                    if (string.IsNullOrEmpty(fileName) == false) {
+                        model.ImageUrl = fileName;
+                    }
+
                     _context.Add(model);
                     await _context.SaveChangesAsync();
 
                 } else {
-                    var category = await _context.Products.FindAsync(id);
+                    var product = await _context.Products.FindAsync(id);
 
-                    category.CategoryProductId = model.CategoryProductId;
-                    category.Name = model.Name;
-                    category.Price = model.Price;
-                    category.Quantity = model.Quantity;
-                    category.Display = model.Display;
-                    category.Description = model.Description;
-                    category.ModifiedDate = DateTime.Now;
+                    product.CategoryProductId = model.CategoryProductId;
+                    product.Name = model.Name;
+                    product.Price = model.Price;
+                    product.Quantity = model.Quantity;
+                    product.Display = model.Display;
+                    product.Description = model.Description;
+                    product.ModifiedDate = DateTime.Now;
 
-                    _context.Update(category);
+                    if (string.IsNullOrEmpty(fileName) == false) {
+                        product.ImageUrl = fileName;
+                    }
+                    _context.Update(product);
                     await _context.SaveChangesAsync();
                 }
 
